e24b92ab1da8c1bec9d3bafd1c5651f4eec89335
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-09-11 17:15:01 +0000

    fix : update

--- src/shared/constantes/team-menbers.ts
+++ src/shared/constantes/team-menbers.ts
@@ -1,3 +1,104 @@
+// export const TEAM_MEMBERS = [
+//   {
+//     name: "Mariam Timité",
+//     role: "Developpeuse full-stack",
+//     image: "/assets/team/IMG_8339.jpeg",
+//   },
+//   {
+//     name: "Kone Assata Nagninyomo",
+//     role: "Developpeuse full-stack",
+//     image: "/assets/team/IMG_8336.jpeg",
+//   },
+//   {
+//     name: "Bouaille Stéphanie Marie-Hélène Essis",
+//     role: "Developpeuse full-stack",
+//     image: "/assets/team/ESSIS.jpg",
+//   },
+//   {
+//     name: "Edwige Annick KOUASSI",
+//     role: "Developpeuse full-stack",
+//     image: "/assets/team/KOUASSI.jpg",
+//   },
+//   {
+//     name: "Akou Beugre Fulgence",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/AKOU.jpg",
+//   },
+//   {
+//     name: "ARSENE KOFFI OROUNLA",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/ASSERNE.jpg",
+//   },
+//   {
+//     name: "GBOKO ANZOUMANAN AMARA",
+//     role: "Developpeur full stack",
+//     statut:"En Poste",
+//     Entreprise: "SC DIGITAL",
+//     image: "/assets/team/amaraG.jpg",
+//     portfolio: "https://my-next-portfolio-pi.vercel.app/"
+//   },
+//   {
+//     name: "Kouakou Younouss Assane",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/ASSANE.jpg",
+//   },
+//   {
+//     name: "SYLVESTRE BENIE",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/BENIE.jpg",
+//   },
+//   {
+//     name: "Kouloman Youssouf COULIBALY",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/COULIBALY.jpg",
+//   },
+//   {
+//     name: "KOFFI KOFFI",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/KOFFI.jpg",
+//   },
+//   {
+//     name: "Salam Jean-Louis Ouedraogo",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/SALAM.jpg",
+//   },
+//   {
+//     name: "Koffi Kouadio jean Alex",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/KOUADIO.jpg",
+//   },
+//   {
+//     name: "Achada ange-junior Abbe",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/LECIBORG.jpg",
+//   },
+//   {
+//     name: "SOUALIO KALO",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/KALO.jpg",
+//   },
+//   {
+//     name: "MOHAMED SALIM RADJI",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/RAJIB.jpg",
+//   },
+//   {
+//     name: "KOFFI YVES PLACIDE KOUAME",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/YVES.jpg",
+//   },
+//   {
+//     name: "Namory Charles TIMITÉ",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/NAMORY.jpg",
+//   },
+//   {
+//     name: "Dandan Gadjro David Israël",
+//     role: "Developpeur full stack",
+//     image: "/assets/team/1.jpg",
+//   },
+// ];
+
 export const TEAM_MEMBERS = [
   {
     name: "Mariam Timité",
@@ -11,7 +112,8 @@ export const TEAM_MEMBERS = [
   },
   {
     name: "Bouaille Stéphanie Marie-Hélène Essis",
-    role: "Developpeuse full-stack",
+    role: "Developpeuse web full-stack & chargée d'etudes",
+    statut:"En Poste",
     image: "/assets/team/ESSIS.jpg",
   },
   {
@@ -26,8 +128,11 @@ export const TEAM_MEMBERS = [
   },
   {
     name: "ARSENE KOFFI OROUNLA",
-    role: "Developpeur full stack",
-    image: "/assets/team/ASSERNE.jpg",
+    role: "Developpeur full stack Junior",
+    statut:"Chargé du Support IT",
+    Entreprise: "TRAFRULE SA",
+    image: "/assets/team/ASSERNE.jpg", 
+    portfolio: "https://arsene-orounla.vercel.app/"
   },
   {
     name: "GBOKO ANZOUMANAN AMARA",
@@ -50,6 +155,8 @@ export const TEAM_MEMBERS = [
   {
     name: "Kouloman Youssouf COULIBALY",
     role: "Developpeur full stack",
+    statut: "Chef d'entreprise",
+    Entreprise: "2K SERVICES SARL",
     image: "/assets/team/COULIBALY.jpg",
   },
   {
@@ -57,10 +164,13 @@ export const TEAM_MEMBERS = [
     role: "Developpeur full stack",
     image: "/assets/team/KOFFI.jpg",
   },
-  {
-    name: "Salam Jean-Louis Ouedraogo",
+{
+    name: "OUEDRAOGO SALAM JEAN-LOUIS",
     role: "Developpeur full stack",
+    statut:"En Poste",
+    Entreprise: "KKS-TECHNOLOGIES",
     image: "/assets/team/SALAM.jpg",
+    portfolio: "https://portfoliosalamouedraogo.vercel.app/"
   },
   {
     name: "Koffi Kouadio jean Alex",
